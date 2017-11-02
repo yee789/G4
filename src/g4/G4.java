dd1755cbd6043cae3f62d5d209677f8f19831fa5
Student <Student@PC100>
2017-11-02 15:42:10 +0800

    my first commit

--- src/g4/G4.java
+++ src/g4/G4.java
@@ -16,6 +16,8 @@ public class G4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Main");
+        System.out.println("First-branch");
     }
     
 }
